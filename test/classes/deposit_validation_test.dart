5cb20f3ef6e3ba4849d25602649ef00a11c04e69
Felipe Sales <soufeliposales@gmail.com>
2021-10-16 18:45:48 -0300

    testing the DepositValidation (value zero, negative and ok)

--- test/classes/deposit_validation_test.dart
+++ test/classes/deposit_validation_test.dart
@@ -1,14 +1,44 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:unitary_tests/classes/deposit_validation.dart';
+import 'package:unitary_tests/error/failure.dart';
+import 'package:unitary_tests/models/operation.dart';
+import 'package:unitary_tests/models/operation_result.dart';
 
 void main() {
-  setUp(() {
-    print('Iniciou o teste');
-  });
-  tearDown(() {
-    print('Terminou o teste');
+  test('Test validation with value zero', () {
+    final depositValidation = DepositValidation();
+    expect(
+      () => depositValidation.validate(
+        balance: 0.0,
+        value: 0.0,
+      ),
+      throwsA(
+        isA<Failure>(),
+      ),
+    );
   });
 
-  test('Test', () {
-    print('Teste');
+  test('Test validation with value is Negative', () {
+    final depositValidation = DepositValidation();
+    expect(
+      () => depositValidation.validate(
+        balance: 0.0,
+        value: -100.0,
+      ),
+      throwsA(
+        isA<Failure>(),
+      ),
+    );
+  });
+
+  test('Test when result is ok', () {
+    final depositValidation = DepositValidation();
+    OperationResult response = depositValidation.validate(
+      balance: 0.0,
+      value: 100.0,
+    );
+    expect(response.result, 100);
+    expect(response.history.type, OperationType.deposit);
+    expect(response.message, 'Depósito realizado com sucesso');
   });
 }
