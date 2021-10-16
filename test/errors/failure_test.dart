6b6fd41ff3df4c854c162829abe1509c8dde12c5
Felipe Sales <soufeliposales@gmail.com>
2021-10-16 19:22:40 -0300

    testing Failure with and without message

---
+++ test/errors/failure_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unitary_tests/error/failure.dart';
+
+void main() {
+  test('Test throw Exception Failure with message', () {
+    void function() {
+      throw Failure(message: 'Falha executada');
+    }
+
+    expect(
+      function,
+      throwsA(
+        isA<Failure>(),
+      ),
+    );
+  });
+
+  test('Test throw Exception Failure without message', () {
+    void function() {
+      throw Failure();
+    }
+
+    expect(
+      function,
+      throwsA(
+        isA<Failure>(),
+      ),
+    );
+  });
+
+  test('Test Failure message', () {
+    final failure = Failure(message: 'Falha Executada');
+    expect(failure.message, 'Falha Executada');
+  });
+
+    test('Test Failure without message', () {
+    final failure = Failure();
+    expect(failure.message, '');
+  });
+}
