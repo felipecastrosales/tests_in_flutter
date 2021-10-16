f503c21f8a1a5a641e65ac08ecd876aa72beebcb
Felipe Sales <soufeliposales@gmail.com>
2021-10-16 01:09:34 -0300

    testing the basic structure for a test, in introduction

---
+++ test/classes/deposit_validation_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {
+    print('Iniciou o teste');
+  });
+  tearDown(() {
+    print('Terminou o teste');
+  });
+
+  test('Test', () {
+    print('Teste');
+  });
+}
